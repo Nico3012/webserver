ac2787a631034c09f390d0ba147b69cedaa288e2
Nico Hörter <nico.m.hoerter@gmail.com>
2024-10-08 14:59:52 +0200

    add latest stuff

--- test7/main.go
+++ test7/main.go
@@ -3,12 +3,39 @@ package main
 import (
 	"crypto/tls"
 	"log"
+	"net"
 	"net/http"
 	"test7/cache"
 
 	"golang.org/x/crypto/acme"
 	"golang.org/x/crypto/acme/autocert"
 )
+
+func startHttp(addr string, handler http.Handler) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = http.Serve(listener, handler)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startHttps(addr string, handler http.Handler, getCertificate func(*tls.ClientHelloInfo) (*tls.Certificate, error)) {
+	listener, err := tls.Listen("tcp", addr, &tls.Config{
+		GetCertificate: getCertificate,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = http.Serve(listener, handler)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	cache := cache.Cache("./cache.json")
@@ -23,21 +50,8 @@ func main() {
 		Cache: cache,
 	}
 
-	listener, err := tls.Listen("tcp", ":5000", &tls.Config{
-		GetCertificate: manager.GetCertificate,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	log.Println("Starting servers")
 
-	httpHandler := http.NewServeMux()
-
-	httpHandler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hallo Welt! This is http"))
-	})
-
-	go http.ListenAndServe(":80", manager.HTTPHandler(httpHandler))
-	log.Fatal(http.Serve(listener, http.FileServer(http.Dir("html"))))
+	go startHttp(":80", manager.HTTPHandler(http.RedirectHandler("https://liquipay.de:5000", http.StatusTemporaryRedirect)))
+	startHttps(":5000", http.FileServer(http.Dir("html")), manager.GetCertificate)
 }
