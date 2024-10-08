10f2de4a23600bb2ca390d8de6827d1e819a7787
Nico Hörter <nico.m.hoerter@gmail.com>
2024-10-08 17:40:40 +0200

    add http functions

--- web_server.go
+++ web_server.go
@@ -2,6 +2,7 @@ package webserver
 
 import (
 	"crypto/tls"
+	"net"
 	"net/http"
 
 	"github.com/Nico3012/webserver/jsoncache"
@@ -9,7 +10,16 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-func listenAndServeTLS(addr string, getCertificate func(hello *tls.ClientHelloInfo) (*tls.Certificate, error), handler http.Handler) error {
+func listenAndServe(addr string, handler http.Handler) error {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	return http.Serve(listener, handler)
+}
+
+func listenAndServeTLS(addr string, handler http.Handler, getCertificate func(hello *tls.ClientHelloInfo) (*tls.Certificate, error)) error {
 	listener, err := tls.Listen("tcp", addr, &tls.Config{
 		GetCertificate: getCertificate,
 	})
@@ -22,16 +32,20 @@ func listenAndServeTLS(addr string, getCertificate func(hello *tls.ClientHelloIn
 
 // exported functions:
 
+func CreateWebServer(addr string, handler http.Handler) error {
+	return listenAndServe(addr, handler)
+}
+
 // this function creates a web server from certificate files
 // the server automatically reacts to certificate file updates
-func CreateWebServer(addr string, certPath string, certKeyPath string, handler http.Handler) {
+func CreateSecureWebServer(addr string, certPath string, certKeyPath string, handler http.Handler) {
 }
 
 // this function creates a web server from certificate authority (ca) files
 // the server automatically reacts to certificate authority (ca) file updates
 // the server automatically renews its certificates before they expire
 // compared to Let’s Encrypt, this function does not need a cache location to store certificates, because self signed certificates can be generated as often as needed
-func CreateWebServerWithSelfSignedCertificate(addr string, caPath string, caKeyPath string, hosts []string, handler http.Handler) {
+func CreateSecureWebServerAndSelfSignedCertificate(addr string, caPath string, caKeyPath string, hosts []string, handler http.Handler) {
 }
 
 // this function creates a web server from Let’s Encrypt certificate authority (ca)
@@ -40,7 +54,7 @@ func CreateWebServerWithSelfSignedCertificate(addr string, caPath string, caKeyP
 // cacheJsonPath is the file path to a json file, which acts like a key- value storage for certificate information
 // compared to self signed, this function needs a cache location to store certificates, because Let’s Encrypt limits the amount of certificates, that can be generated within a few days
 // testing boolean indicates if the staging environment is used
-func CreateWebServerWithLetsEncryptCertificate(addr string, jsonCachePath string, testing bool, hosts []string, handler http.Handler) func(http.Handler) http.Handler {
+func CreateSecureWebServerAndLetsEncryptCertificate(addr string, jsonCachePath string, testing bool, hosts []string, handler http.Handler) func(http.Handler) http.Handler {
 	jsonCache := jsoncache.JsonCache(jsonCachePath)
 
 	directoryURL := "https://acme-v02.api.letsencrypt.org/directory"
@@ -60,7 +74,7 @@ func CreateWebServerWithLetsEncryptCertificate(addr string, jsonCachePath string
 	}
 
 	// go routine
-	listenAndServeTLS(addr, manager.GetCertificate, handler)
+	listenAndServeTLS(addr, handler, manager.GetCertificate)
 
 	return manager.HTTPHandler
 }
